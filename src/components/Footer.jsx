dd3eca066316fa50fb549ca4e6bb0a24ef54e89f
handod <69374221+handod@users.noreply.github.com>
2021-02-07 01:50:54 +0200

    first

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; Copyright David Saidon 2020</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+function Header() {
+  return (
+    <header>
+      <h1>
+        <EventNoteIcon /> Saidon Keeper App
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
