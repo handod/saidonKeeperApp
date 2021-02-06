d5da122c7796f9be7d0f45e0391e4433ea7f31ee
handod <69374221+handod@users.noreply.github.com>
2021-02-07 01:53:10 +0200

    Updates

---
+++ precache-manifest.d5526f7a7f9e1cbb5f07c5b9a3c88a3b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ee10a7478edba7a98bef42847ce49fb",
+    "url": "/handod.github.io/saidonKeeperApp/index.html"
+  },
+  {
+    "revision": "388842b7ff79d71f3c8b",
+    "url": "/handod.github.io/saidonKeeperApp/static/js/2.828b091c.chunk.js"
+  },
+  {
+    "revision": "7b4e5ae3552dd0f3d3bb",
+    "url": "/handod.github.io/saidonKeeperApp/static/js/main.e302f1a5.chunk.js"
+  },
+  {
+    "revision": "4c6dc6cb8a58a7ea716b",
+    "url": "/handod.github.io/saidonKeeperApp/static/js/runtime-main.9262007e.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/handod.github.io/saidonKeeperApp/precache-manifest.d5526f7a7f9e1cbb5f07c5b9a3c88a3b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/handod.github.io/saidonKeeperApp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
